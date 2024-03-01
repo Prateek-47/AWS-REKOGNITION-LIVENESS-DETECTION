478653e066681ab2602e747dc14d7562daca8cc6
Prateek sharma <102501633+Prateek-47@users.noreply.github.com>
2024-03-01 12:51:14 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,55 @@
+// Import necessary modules from Angular and AWS Amplify
 import { Component, OnInit } from '@angular/core';
 import awsconfig from './../aws-exports';
 import { Auth, Amplify } from 'aws-amplify';
 import { Hub } from 'aws-amplify';
 
+// Define the Angular component
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  // Component properties
   title = 'aws-rekognition-liveness-detection-angular';
   load_face_live = false;
+
+  // Initialize component on Angular's OnInit lifecycle event
   ngOnInit(): void {
+    // Configure AWS Amplify with the provided AWS configuration
     Amplify.configure(awsconfig);
     Auth.configure(awsconfig);
-    Hub.listen('auth', this.listener)
+
+    // Listen for 'auth' events using the Hub module
+    Hub.listen('auth', this.listener);
+
+    // Set a flag to indicate that face live loading is in progress
     this.load_face_live = true;
-    console.log('component-loaded')
+
+    // Log a message to the console indicating that the component has been loaded
+    console.log('component-loaded');
   }
 
+  // Method to sign the user out
   signOut(): void {
+    // Set the face live loading flag to false
     this.load_face_live = false;
+
+    // Use a setTimeout to delay the actual sign-out operation by 3 milliseconds
     setTimeout(() => {
       Auth.signOut();
     }, 3);
   }
 
+  // Event listener for authentication events
   listener = (data) => {
     switch (data.payload.event) {
+      // When a user signs in, log a message and set the face live loading flag to true
       case 'signIn':
         console.log('user signed in');
         this.load_face_live = true;
         break;
     }
   };
-
-  
 }
