a38a5c801dc057615bacc25ae1f5eba90b2b0624
Prateek sharma <102501633+Prateek-47@users.noreply.github.com>
2024-03-01 12:50:33 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+// Import necessary modules from Angular
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Define an empty array to hold route configurations
 const routes: Routes = [];
 
+// NgModule decorator to define the routing configuration for the Angular application
 @NgModule({
+  // Import the RouterModule and configure it with the defined routes
   imports: [RouterModule.forRoot(routes)],
+  // Export the configured RouterModule to make it available for use in other modules
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
